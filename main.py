4f5340e5d8f0bb6c1f83044d7cfb89544a4481c3
jimmy <jimmy.snowden@gmail.com>
2021-05-15 09:55:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,2 @@
+### https://docs.python.org/3/library/datetime.html for date/time codes to manipulate date/time data.
+### https://www.highcharts.com/docs/chart-and-series-types/areaspline-chart for making all sorts of charts.
